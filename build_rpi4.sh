eecbf74c9d0118897c73f69b4dfda9826a2a8586
Paulo Barradas <paulo.barradas@gmail.com>
2023-04-15 21:02:43 +0100

    OpenWRT 22.03.4
    OpenWRT 22.03.4 test

--- build_rpi4.sh
+++ build_rpi4.sh
@@ -25,13 +25,13 @@ sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=.*/CONFIG_TARGET_KERNEL_PARTSIZE=$KERNEL
 sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE/g" .config
 
 make image  PROFILE="rpi-4" \
-           PACKAGES="base-files bcm27xx-gpu-fw busybox ca-bundle cypress-firmware-43455-sdio \
-                    cypress-nvram-43455-sdio-rpi-4b dnsmasq dropbear e2fsprogs firewall4 \
-                    fstools iwinfo kmod-brcmfmac kmod-fs-vfat kmod-nft-offload kmod-nls-cp437 \
-                    kmod-nls-iso8859-1 kmod-sound-arm-bcm2835 kmod-sound-core kmod-usb-hid \
-                    kmod-usb-net-lan78xx libc libgcc libustream-wolfssl logd mkf2fs mtd netifd \
-                    nftables odhcp6c odhcpd-ipv6only opkg partx-utils ppp ppp-mod-pppoe procd \
-                    procd-seccomp procd-ujail uci uclient-fetch urandom-seed wpad-basic-wolfssl \
-                    nano wget-ssl kmod-usb-net-rtl8152 kmod-ath9k-htc luci luci-ssl luci-compat luci-lib-ipkg" \
+           PACKAGES="base-files bcm27xx-gpu-fw busybox ca-bundle cypress-firmware-43455-sdio cypress-nvram-43455-sdio-rpi-4b \
+                    dnsmasq dropbear e2fsprogs firewall4 fstools iwinfo kmod-brcmfmac kmod-fs-vfat kmod-nft-offload \
+                    kmod-nls-cp437 kmod-nls-iso8859-1 kmod-sound-arm-bcm2835 kmod-sound-core kmod-usb-hid kmod-usb-net-lan78xx libc \
+                    libgcc libustream-wolfssl logd mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only opkg partx-utils ppp ppp-mod-pppoe \
+                    procd procd-seccomp procd-ujail uci uclient-fetch urandom-seed wpad-basic-wolfssl kmod-usb-storage luci-compat \
+                    luci-lib-ipkg cfdisk resize2fs kmod-usb-storage-extras kmod-usb-storage-uas usbutils block-mount ntfs-3g \
+                    kmod-fs-ext4 wget-ssl lsblk luci-compat luci-lib-ipkg kmod-usb-net-rtl8152 luci luci-ssl kmod-ath9k-htc \
+                    nano dockerd luci-app-dockerman adblock luci-app-adblock" \
             FILES="${BASEDIR}/files/" \
             BIN_DIR="${OUTPUT}"
